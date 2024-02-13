1aba88b730c4e3a33fad816bbbbbb703122de92e
Nicolas Monnier <nicolas.monnier44@gmail.com>
2024-02-13 10:39:03 +0100

    Successfuly interpolate real data on cube model

--- generate_real_data_2.py
+++ generate_real_data_2.py
@@ -81,11 +81,14 @@ srfs = instru.get_srf(
     step,
 )
 
+
+margin=100
+maps_shape = (maps.shape[0], maps.shape[1]+margin*2, maps.shape[2]+margin*2)
 step_Angle = Angle(step, u.arcsec)
-alpha_axis = np.arange(maps.shape[1]) * step_Angle.degree
-beta_axis = np.arange(maps.shape[2]) * step_Angle.degree
-alpha_axis -= np.mean(alpha_axis)
-beta_axis -= np.mean(beta_axis)
+origin_alpha_axis = np.arange(maps_shape[1]) * step_Angle.degree
+origin_beta_axis = np.arange(maps_shape[2]) * step_Angle.degree
+origin_alpha_axis -= np.mean(origin_alpha_axis)
+origin_beta_axis -= np.mean(origin_beta_axis)
 """ alpha_axis += +miri.ch1a.fov.origin.alpha
 beta_axis += +miri.ch1a.fov.origin.beta """
 
@@ -118,120 +121,155 @@ main_pointing = instru.Coord(0, 0)
 pointings = instru.CoordList(c + main_pointing for c in ch1_dither).pix(step)
 
 
+import os
+directory = os.fsencode('/home/nmonnier/Data/JWST/Orion_bar/single_file/')
+res_dict = {}
+for of in range(2,4):
+    res_dict[of] = 0
+    for file in os.listdir(directory):
 
-#%% Real data
-path = '/home/nmonnier/Data/JWST/Orion_bar/ChannelCube_ch2-short_s3d.fits'
-hdul = fits.open(path)
-cube = hdul[1].data
+        #%% Real data
+        #path = '/home/nmonnier/Data/JWST/Orion_bar/single_file/ChannelCube_ch1-short_s3d_02101_00001.fits'
+        hdul = fits.open(directory+file)
+        cube = hdul[1].data
 
-im = hdul[1].data
-
-im[0] = gray[:,:29]
-
-w = wcs.WCS(hdul[1].header)
-zz,xx,yy = np.indices(im.shape)
-
-a, b, c = w.wcs_pix2world(xx, yy, zz, 1)
-
-wavelength_axis = c[:,0,0]
-ra_map = a[0,:,:]
-dec_map = b[0,:,:]
-
-of = 3 # Oversapling_factor -> To match the size of the model
-
-of_ra_map = np.zeros(((ra_map.shape[0]-2)*of, (ra_map.shape[1]-2)*of), dtype=ra_map.dtype)
-of_dec_map = np.zeros(((dec_map.shape[0]-2)*of, (dec_map.shape[1]-2)*of), dtype=dec_map.dtype)
-
-# Oversample im with 0 padding
-# Then dupplicate value to 0 values
-of_im = np.zeros((im.shape[0], im.shape[1]*of, im.shape[2]*of), dtype=im.dtype)
-of_im[:, ::3, ::3] = im
-n_of_im = np.zeros((of_im.shape[0], (of_im.shape[1]-2*of)*of, (of_im.shape[2]-2*of)*of), dtype=im.dtype)
-n_of_im = of_im[:, of:-of, of:-of]
-of_im = n_of_im
-
-kern = np.ones((of,of))
-kern = np.pad(kern, (2,0))
-for l in range(of_im.shape[0]):
-    of_im[l] = scipy.signal.convolve2d(of_im[l], kern, 'same')
+        im = hdul[1].data
 
 
+        gray_gradient = np.arange(im[0].shape[0])
+        gray_gradient = gray_gradient*255/(im[0].shape[0]-1)
+        gray_gradient = np.tile(gray_gradient.reshape((-1, 1)), (1, im[0].shape[1]))
 
-ra_step_y = ra_map[5,6] - ra_map[5,5]
-of_ra_step_y = ra_step_y/3
-ra_step_x = ra_map[6,5] - ra_map[5,5]
-of_ra_step_x = ra_step_x/3
+        #im[0] = gray_gradient#gray[:,:29]
 
-dec_step_y = dec_map[5,6] - dec_map[5,5]
-of_dec_step_y = dec_step_y/3
-dec_step_x = dec_map[6,5] - dec_map[5,5]
-of_dec_step_x = dec_step_x/3
+        w = wcs.WCS(hdul[1].header)
+        zz,xx,yy = np.indices(im.shape)
 
-short_ra_map = np.delete(ra_map, 0, 0)
-short_ra_map = np.delete(short_ra_map, 0, 1)
-short_ra_map = np.delete(short_ra_map, -1, 0)
-short_ra_map = np.delete(short_ra_map, -1, 1)
+        a, b, c = w.wcs_pix2world(xx, yy, zz, 1)
 
-short_dec_map = np.delete(dec_map, 0, 0)
-short_dec_map = np.delete(short_dec_map, 0, 1)
-short_dec_map = np.delete(short_dec_map, -1, 0)
-short_dec_map = np.delete(short_dec_map, -1, 1)
+        wavelength_axis = c[:,0,0]
+        ra_map = a[0,:,:]
+        dec_map = b[0,:,:]
 
-### 
-for i in range(ra_map.shape[0]-2):
-    for j in range(ra_map.shape[1]-2):
-        of_ra_map[i*of, j*of] = ra_map[i+1,j+1]
+        #of = 2 # Oversapling_factor -> To match the size of the model
 
-for i in range(dec_map.shape[0]-2):
-    for j in range(dec_map.shape[1]-2):
-        of_dec_map[i*of, j*of] = dec_map[i+1,j+1]
+        of_ra_map = np.zeros(((ra_map.shape[0]-2)*of, (ra_map.shape[1]-2)*of), dtype=ra_map.dtype)
+        of_dec_map = np.zeros(((dec_map.shape[0]-2)*of, (dec_map.shape[1]-2)*of), dtype=dec_map.dtype)
 
-### Interpolate unknow values
-for i in range(of_ra_map.shape[0]):
-    for j in range(of_ra_map.shape[1]):
-        if i%of !=0:
-            of_ra_map[i, j] = of_ra_map[i-(i%of), j] + of_ra_step_x*(i%of)
-        else:
-            if j%of != 0:
-                of_ra_map[i, j] = of_ra_map[i, j-(j%of)] + of_ra_step_y*(j%of)
+        # Oversample im with 0 padding
+        # Then dupplicate value to 0 values
+        of_im = np.zeros((im.shape[0], im.shape[1]*of, im.shape[2]*of), dtype=im.dtype)
+        of_im[:, ::of, ::of] = im
+        n_of_im = np.zeros((of_im.shape[0], (of_im.shape[1]-2*of)*of, (of_im.shape[2]-2*of)*of), dtype=im.dtype)
+        n_of_im = of_im[:, of:-of, of:-of]
+        of_im = n_of_im
 
-for i in range(of_dec_map.shape[0]):
-    for j in range(of_dec_map.shape[1]):
-        if i%of !=0:
-            of_dec_map[i, j] = of_dec_map[i-(i%of), j] + of_dec_step_x*(i%of)
-        else:
-            if j%of != 0:
-                of_dec_map[i, j] = of_dec_map[i, j-(j%of)] + of_dec_step_y*(j%of)
+        kern = np.ones((of,of))
+        kern = np.pad(kern, (of-1,0))
+        for l in range(of_im.shape[0]):
+            of_im[l] = scipy.signal.convolve2d(of_im[l], kern, 'same')
 
 
-wavelength_idx = np.arange(len(wavelength_axis)-1)
-of_im = of_im[:-1] # Because miri wavelength is not the same size as the real data
 
-TwoD_of_im = of_im[0].ravel()
+        ra_step_y = ra_map[5,6] - ra_map[5,5]
+        of_ra_step_y = ra_step_y/of
+        ra_step_x = ra_map[6,5] - ra_map[5,5]
+        of_ra_step_x = ra_step_x/of
+
+        dec_step_y = dec_map[5,6] - dec_map[5,5]
+        of_dec_step_y = dec_step_y/of
+        dec_step_x = dec_map[6,5] - dec_map[5,5]
+        of_dec_step_x = dec_step_x/of
+
+        short_ra_map = np.delete(ra_map, 0, 0)
+        short_ra_map = np.delete(short_ra_map, 0, 1)
+        short_ra_map = np.delete(short_ra_map, -1, 0)
+        short_ra_map = np.delete(short_ra_map, -1, 1)
+
+        short_dec_map = np.delete(dec_map, 0, 0)
+        short_dec_map = np.delete(short_dec_map, 0, 1)
+        short_dec_map = np.delete(short_dec_map, -1, 0)
+        short_dec_map = np.delete(short_dec_map, -1, 1)
+
+        ### 
+        for i in range(ra_map.shape[0]-2):
+            for j in range(ra_map.shape[1]-2):
+                of_ra_map[i*of, j*of] = ra_map[i+1,j+1]
+
+        for i in range(dec_map.shape[0]-2):
+            for j in range(dec_map.shape[1]-2):
+                of_dec_map[i*of, j*of] = dec_map[i+1,j+1]
+
+        ### Interpolate unknow values
+        for i in range(of_ra_map.shape[0]):
+            for j in range(of_ra_map.shape[1]):
+                if i%of !=0:
+                    of_ra_map[i, j] = of_ra_map[i-(i%of), j] + of_ra_step_x*(i%of)
+                else:
+                    if j%of != 0:
+                        of_ra_map[i, j] = of_ra_map[i, j-(j%of)] + of_ra_step_y*(j%of)
+
+        for i in range(of_dec_map.shape[0]):
+            for j in range(of_dec_map.shape[1]):
+                if i%of !=0:
+                    of_dec_map[i, j] = of_dec_map[i-(i%of), j] + of_dec_step_x*(i%of)
+                else:
+                    if j%of != 0:
+                        of_dec_map[i, j] = of_dec_map[i, j-(j%of)] + of_dec_step_y*(j%of)
 
 
-### Interpolation 2D image
-points = np.vstack(
-    [
-        of_ra_map.ravel(),
-        of_dec_map.ravel()         
-    ]
-    ).T
+        wavelength_idx = np.arange(len(wavelength_axis)-1)
+        of_im = of_im[:-1] # Because miri wavelength is not the same size as the real data
 
-alpha_axis += hdul[1].header['CRVAL1']
-beta_axis += hdul[1].header['CRVAL2']
-xi = np.vstack(
-    [
-        np.tile(alpha_axis.reshape((1, -1)), (len(beta_axis), 1)).ravel(), 
-        np.tile(beta_axis.reshape((-1, 1)), (1, len(alpha_axis))).ravel()
-    ]
-    ).T
+        TwoD_of_im = of_im[0].ravel()
 
-from scipy.interpolate import griddata
-interpolated_im = griddata(points, TwoD_of_im, xi, method='linear').reshape(251,251)
 
-plt.imshow(np.flipud(np.fliplr(interpolated_im.reshape(251,251))))
-plt.show()
+        ### Interpolation 2D image
+        points = np.vstack(
+            [
+                of_ra_map.ravel(),
+                of_dec_map.ravel()         
+            ]
+            ).T
+
+        alpha_axis = origin_alpha_axis + hdul[1].header['CRVAL1']
+        beta_axis = origin_beta_axis + hdul[1].header['CRVAL2']
+        xi = np.vstack(
+            [
+                np.tile(alpha_axis.reshape((1, -1)), (len(beta_axis), 1)).ravel(), 
+                np.tile(beta_axis.reshape((-1, 1)), (1, len(alpha_axis))).ravel()
+            ]
+            ).T
+
+        from scipy.interpolate import griddata
+        interpolated_im = griddata(points, TwoD_of_im, xi, method='linear').reshape(maps_shape[1],maps_shape[2])
+
+        print(f"Fits {file}")
+        print(f"Original image max is {np.nanmax(im[0])}")
+        print(f"Oversampled image max is {np.nanmax(of_im[0])}")
+        print(f"Interpolated image max is {np.nanmax(interpolated_im)}\n")
+
+        print(f"Original image min is {np.nanmin(im[0])}")
+        print(f"Oversampled image min is {np.nanmin(of_im[0])}")
+        print(f"Interpolated image min is {np.nanmin(interpolated_im)}\n")
+
+        print(f"Relative Difference min is {(np.nanmin(im[0])-np.nanmin(interpolated_im))/np.nanmin(im[0]) *100} %")
+        print(f"Relative Difference max is {(np.nanmax(im[0])-np.nanmax(interpolated_im))/np.nanmax(im[0]) *100} %")
+        res_dict[of] += (np.nanmin(im[0])-np.nanmin(interpolated_im))/np.nanmin(im[0]) *100
+        print("\n\n")
+        hdul.close()
+
+
+
+    """ plt.figure()
+    plt.imshow(of_im[0])
+    plt.colorbar()
+    plt.figure()
+    plt.imshow(np.flipud(np.fliplr(interpolated_im.reshape(maps_shape[1],maps_shape[2]))))
+    plt.colorbar()
+    plt.show() """
+
+print(res_dict)
 
 ### Inteprolation 3D image
 """ interpolated_cube = np.zeros((len(wavelength_idx)-1, 251, 251), dtype=interpolated_im.dtype)
