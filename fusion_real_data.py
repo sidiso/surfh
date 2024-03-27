Fix Rotation ch2 + debugging

fusion_real_data.py
@@ -23,6 +23,7 @@ from pathlib import Path
 import click
 
 import datetime
+import aljabr
 
 from surfh.Models import spectro
 from surfh.Models import spectrolmm
@@ -40,6 +41,8 @@ from surfh.Models import spectrolmm
 @click.option('--value_init', default=100, type=click.FLOAT)
 def launch_fusion(data_dir, res_dir, hyper, sim_data, niter, multi_chan, verbose, method, margin, value_init):
 
+
+
     if multi_chan is True:
         print("Multi channels/bands fusion")
         fits_directory    = data_dir + 'All_bands_fits/'
@@ -50,6 +53,7 @@ def launch_fusion(data_dir, res_dir, hyper, sim_data, niter, multi_chan, verbose
         fits_directory      = data_dir + 'Single_fits/'
         slices_directory    = data_dir + 'Single_numpy_slices/'
         psf_directory       = data_dir + 'All_bands_psf/'
+        mask_directory      = data_dir + 'Single_mask/'
 
     mu = str(hyper)
     mu = mu.split('.')
@@ -133,7 +137,7 @@ def launch_fusion(data_dir, res_dir, hyper, sim_data, niter, multi_chan, verbose
     wavel_axis = wavel_axis[::tpl_ss]
     #spsf = utils.gaussian_psf(wavel_axis, step_Angle.degree)
     spsf = np.load(psf_directory + 'psfs_pixscale0.025_fov11.25_date_300123.npy')
-    spsf = spsf[:, 100:351, 100:351]
+    spsf = spsf[:, (100-margin):(351+margin), (100-margin):(351+margin)]
     print("SHAPE PSF ARE ", spsf.shape)
 
     if "sotf" not in globals():
@@ -162,6 +166,9 @@ def launch_fusion(data_dir, res_dir, hyper, sim_data, niter, multi_chan, verbose
 
         list_data.append(data)
 
+        #mask = np.load(mask_directory + Path(file).stem + '.npy')
+        #mask = np.ones_like(mask)
+
 
     origin_alpha_axis += list_channels[0].fov.origin.alpha
     origin_beta_axis += list_channels[0].fov.origin.beta
@@ -176,6 +183,7 @@ def launch_fusion(data_dir, res_dir, hyper, sim_data, niter, multi_chan, verbose
         tpl,
         verbose=verbose,
         serial=False,
+        mask=0,
     )
 
 
@@ -188,9 +196,7 @@ def launch_fusion(data_dir, res_dir, hyper, sim_data, niter, multi_chan, verbose
         print("Real data selection")
         y_data = list_data[0].ravel()
     
-
-    mask = utils.make_mask_FoV(spectro.sliceToCube(y_data))
-    
+    mask = utils.make_mask_FoV(spectro.sliceToCube(y_data)) 
 
     quadCrit = fusion.QuadCriterion_MRS(mu_spectro=1, 
                                         y_spectro=np.copy(y_data), 
@@ -199,6 +205,10 @@ def launch_fusion(data_dir, res_dir, hyper, sim_data, niter, multi_chan, verbose
                                         mask=mask,
                                         printing=True, 
                                         gradient="separated")
+    print("Dot Test : ")
+    print(aljabr.dottest(quadCrit.npdiff_r))
+    print(aljabr.dottest(quadCrit.npdiff_c))
+    print("---------------")
     res = quadCrit.run_method(method, niter, perf_crit = 1, calc_crit=True, value_init=value_init)
 
     """

generate_real_data.py
@@ -33,6 +33,7 @@ main_directory = '/home/nmonnier/Data/JWST/Orion_bar/'
 fits_directory = main_directory + 'Single_fits'
 numpy_directory = main_directory + 'Single_numpy'
 numpy_slices_directory = main_directory + 'Single_numpy_slices'
+mask_directory = main_directory + 'Single_mask'
 
 
 
@@ -80,7 +81,7 @@ if "sotf" not in globals():
 """
 Set Cube coordinate.
 """
-margin=40
+margin=0
 maps_shape = (maps.shape[0], maps.shape[1]+margin*2, maps.shape[2]+margin*2)
 step_Angle = Angle(step, u.arcsec)
 origin_alpha_axis = np.arange(maps_shape[1]) * step_Angle.degree
@@ -106,6 +107,7 @@ for file in os.listdir(fits_directory):
     filename = Path(file).stem
     numpy_file = Path(numpy_directory + '/' + filename + '.npy')
     numpy_slices_file = Path(numpy_slices_directory + '/' + filename + '.npy')
+    mask_file = Path(mask_directory + '/' + filename + '.npy')
 
     print("------------------")
     print(numpy_file)
@@ -120,6 +122,12 @@ for file in os.listdir(fits_directory):
 
 
         im = hdul[1].data
+
+        # Uncomment to get strongh masking
+        im[:,0:4, :] = np.NaN
+        im[:,-4:-1, :] = np.NaN
+        im[:, :, 0:4] = np.NaN
+        im[:, :, -4:-1] = np.NaN
 
         # Get wcs metadata from fits header
         w = wcs.WCS(hdul[1].header)
@@ -254,9 +262,14 @@ spectro = spectro.Spectro(
 )
 
 slices = spectro.channels[0].realData_cubeToSlice(interpolated_cube)
-#np.save(numpy_slices_file, slices)
+np.save(numpy_slices_file, slices)
 slices[np.where(np.isnan(slices))] = 0
 ncube = spectro.channels[0].realData_sliceToCube(slices, interpolated_cube.shape)
+
+# mask = utils.make_mask_FoV(ncube)
+# np.save(mask_file, mask)
+# plt.imshow(mask)
+# plt.show()
 
 plt.figure()
 plt.imshow(interpolated_cube[50])

surfh/Models/spectrolmm.py
@@ -30,6 +30,7 @@ from surfh.Models import instru, channel
 
 from surfh.Others import shared_dict
 from surfh.Others.AsyncProcessPoolLight import APPL
+from surfh.ToolsDir import utils
 
 import matplotlib.pyplot as plt
 
@@ -47,6 +48,7 @@ class SpectroLMM(LinOp):
         templates: array,
         verbose: bool = True,
         serial: bool = False,
+        mask = array,
     ):
 
         self.wavel_axis = wavel_axis
@@ -87,6 +89,7 @@ class SpectroLMM(LinOp):
             for srf, instr in zip(srfs, instrs)
         ]
 
+        self.mask = mask
         self._idx = np.cumsum([0] + [np.prod(chan.oshape) for chan in self.channels])
         self.imshape = (len(alpha_axis), len(beta_axis))
         self.tpls_shape = self.tpls.shape
@@ -174,13 +177,15 @@ class SpectroLMM(LinOp):
         if self.verbose:
             logger.info(f"Spatial blurring^T : IDFT2({tmp.shape})")
         cube = idft(tmp * self.sotf.conj(), self.imshape)
-        return np.concatenate(
+        tmp = np.concatenate(
             [
                 np.sum(cube * tpl[..., np.newaxis, np.newaxis], axis=0)[np.newaxis, ...]
                 for tpl in self.tpls
             ],
             axis=0,
         )
+        # tmp2 = utils.apply_mask_FoV(self.mask, tmp)
+        return tmp
 
 
     def sliceToCube(self, slices):

surfh/ToolsDir/fusion.py
@@ -9,7 +9,9 @@ from surfh.ToolsDir import utils
 
 from scipy.signal import convolve2d as conv2
 
+import matplotlib.pyplot as plt
 
+import inspect
 
 class NpDiff_r(aljabr.LinOp): # dim = 3
     def __init__(self, maps_shape, mask):
@@ -22,12 +24,20 @@ class NpDiff_r(aljabr.LinOp): # dim = 3
     def forward(self, x):
         x_masked = utils.apply_mask_FoV(self.mask, x)
 
-        Dx_masked = - np.diff(np.pad(x_masked, ((0, 0), (1, 0), (0, 0)), 'wrap'), axis=1)
-        Dx_masked[np.where(np.isnan(Dx_masked))] = 0
+        Dx_masked = - np.diff(np.pad(x, ((0, 0), (1, 0), (0, 0)), 'wrap'), axis=1)
+        
+        # Dx_masked[np.where(np.isnan(Dx_masked))] = 0
         return Dx_masked
 
-    def adjoint(self, y):       
-        return np.diff(np.pad(y, ((0, 0), (0, 1), (0, 0)), 'wrap'), axis=1)
+    def adjoint(self, y):    
+        y_masked = utils.apply_mask_FoV(self.mask, y)
+
+        Dy_masked = np.diff(np.pad(y, ((0, 0), (0, 1), (0, 0)), 'wrap'), axis=1)
+
+        #Dy_masked = utils.apply_mask_FoV(self.mask, Dy_masked)
+        # Dy_masked[np.where(np.isnan(Dy_masked))] = 0
+
+        return Dy_masked
 
 class NpDiff_c(aljabr.LinOp): # dim = 3
     def __init__(self, maps_shape, mask):
@@ -40,8 +50,8 @@ class NpDiff_c(aljabr.LinOp): # dim = 3
     def forward(self, x):
         x_masked = utils.apply_mask_FoV(self.mask, x)
 
-        Dx_masked = - np.diff(np.pad(x_masked, ((0, 0), (0, 0), (1, 0)), 'wrap'), axis=2)        
-        Dx_masked[np.where(np.isnan(Dx_masked))] = 0
+        Dx_masked = - np.diff(np.pad(x, ((0, 0), (0, 0), (1, 0)), 'wrap'), axis=2)        
+        # Dx_masked[np.where(np.isnan(Dx_masked))] = 0
         return Dx_masked   
 
     def adjoint(self, y):
@@ -141,6 +151,9 @@ class QuadCriterion_MRS:
             hyper=self.mu_spectro,
             name="Spectro",
         )
+        print("Spectro data adeq")
+        print(spectro_data_adeq)
+        print("-----------")
 
         if self.gradient == "joint":  # regularization term with joint gradients
             prior = QuadObjective(
@@ -183,8 +196,16 @@ class QuadCriterion_MRS:
                 perf_crit_with_reshape(res)
 
 
-        list_obj = [spectro_data_adeq] + prior
+        print("Prior")
+        print(prior[0].hyper)
+        print(prior[1].hyper)
         
+        print("---------")
+        
+        list_obj = [spectro_data_adeq] + prior
+        print("list_obj")
+        print(list_obj)
+        print("---------")
         t1 = time.time()
 
         if method == 'lcg':

surfh/ToolsDir/utils.py
@@ -23,6 +23,7 @@ import numpy as np
 import udft
 import scipy as sp
 
+import matplotlib.pyplot as plt
 
 
 def gaussian_psf(wavel_axis, step, D=6.5):
@@ -90,6 +91,13 @@ def make_mask_FoV(cube, tol=10):
     return mask
 
 def apply_mask_FoV(mask, cube):
+    # plt.figure()
+    # plt.imshow(cube[2])
+    # plt.colorbar()
     masked = mask[np.newaxis,...] * cube
-    masked[np.where(masked == 0)] = np.NaN
+    # plt.figure()
+    # plt.imshow(masked[2])
+    # plt.colorbar()
+    # plt.show()
+    #masked[np.where(masked == 0)] = np.NaN
     return masked

test_script.py
@@ -26,6 +26,8 @@ fits_directory = main_directory + 'Single_fits/'
 numpy_directory = main_directory + 'Single_numpy/'#
 slices_directory = main_directory + 'Single_numpy_slices/'
 
+filename = 'ChannelCube_ch_2_short_s3d_0210f_00001'
+
 
 def orion():
     """Rerturn maps, templates, spatial step and wavelength"""
@@ -90,8 +92,6 @@ main_pointing = instru.Coord(0, 0) # Set the main pointing from the instrument F
 pointings = instru.CoordList([main_pointing])#.pix(step_Angle.degree) # We do not use dithering for first tests
 channels = []
 
-filename = 'ChannelCube_ch_1_long_s3d_02111_00001'
-
 chan = realmiri.get_IFU(fits_directory + filename + '.fits')
 channels.append(chan)
 cube = np.load(numpy_directory + filename + '.npy')
@@ -111,7 +111,7 @@ origin_beta_axis += channels[0].fov.origin.beta
 #     serial=False,
 # )
 
-# data = np.load('/home/nmonnier/Data/JWST/Orion_bar/Single_numpy_slices/ChannelCube_ch_1_long_s3d_02111_00001.npy')
+# data = np.load('/home/nmonnier/Data/JWST/Orion_bar/Single_numpy_slices/' + filename +'.npy')
 # data[np.where(np.isnan(data))] = 0
 # test = spectro.sliceToCube(data)
 