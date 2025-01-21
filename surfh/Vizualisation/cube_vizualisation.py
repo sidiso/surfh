70006dea1b7d3c688ba56c6a0d3bae043766040b
Nicolas Monnier <nicolas.monnier44@gmail.com>
2025-01-21 11:50:12 +0100

    Correct vizualisation

--- surfh/Vizualisation/cube_vizualisation.py
+++ surfh/Vizualisation/cube_vizualisation.py
@@ -3,11 +3,13 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 
 def plot_cube(cube, wavelength_cube):
-    
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from matplotlib.widgets import Slider
+
     if cube.shape[0] != 4:
-        idx = np.where(np.sum(cube, axis=(1,2)) != 0)[0]
+        idx = np.where(np.sum(cube, axis=(1, 2)) != 0)[0]
         nzero_slice = slice(idx[0], idx[-1])
-        print(nzero_slice)
         cube = cube[nzero_slice, ...]
         wavelength_cube = wavelength_cube[nzero_slice]
 
@@ -21,8 +23,8 @@ def plot_cube(cube, wavelength_cube):
 
     # Display the initial slice
     slice_plot = ax.imshow(cube[initial_lambda, :, :], cmap='viridis')
-    ax.set_title(f'Lambda slice: {initial_lambda}')
-    
+    ax.set_title(f'Lambda slice: {wavelength_cube[initial_lambda]}')
+
     # Add a colorbar
     cbar = plt.colorbar(slice_plot, ax=ax)
 
@@ -33,26 +35,30 @@ def plot_cube(cube, wavelength_cube):
     # Update function to be called when the slider is changed
     def update(val):
         lambda_index = int(slider.val)
-        # Update the image data
         new_slice = cube[lambda_index, :, :]
+
+        # Update the image data
         slice_plot.set_data(new_slice)
-        
-        # Update the color limits for the current slice
-        slice_plot.set_clim(vmin=np.min(new_slice), vmax=np.max(new_slice))
 
-        # Redraw the colorbar with the new limits
-        cbar.update_normal(slice_plot)
+        # Update the color limits dynamically
+        vmin, vmax = np.nanmin(new_slice), np.nanmax(new_slice)
+        slice_plot.set_clim(vmin, vmax)
 
-        # Update the title to show the current lambda slice
+        # Redraw the colorbar (uses the ScalarMappable limits)
+        cbar.mappable.set_clim(vmin, vmax)
+        cbar.draw_all()
+
+        # Update the title
         ax.set_title(f'Lambda slice: {wavelength_cube[lambda_index]}')
-        
-        # Redraw the figure canvas
+
+        # Redraw the canvas
         fig.canvas.draw_idle()
 
     # Attach the update function to the slider
     slider.on_changed(update)
 
     plt.show()
+
 
 
 
@@ -81,7 +87,7 @@ def plot_two_cubes(cube_fusion, wavelength_fusion, cube_pipeline, wavelength_pip
 
 
     # Initial lambda index
-    initial_lambda = 0
+    initial_lambda = wavelength_fusion[0]
     nLambda = cube_fusion.shape[0]
 
     # Create a figure and axis
@@ -89,21 +95,23 @@ def plot_two_cubes(cube_fusion, wavelength_fusion, cube_pipeline, wavelength_pip
     plt.subplots_adjust(left=0.1, bottom=0.25)  # Adjust the subplot to make space for the slider
 
     # Display the initial slice
-    slice_plot1 = axes[0].imshow(cube_fusion[initial_lambda, :, :], cmap='viridis')
-    slice_plot2 = axes[1].imshow(viz_pipeline_cube[initial_lambda, :, :], cmap='viridis')
+    slice_plot1 = axes[0].imshow(cube_fusion[0, :, :], cmap='viridis')
+    slice_plot2 = axes[1].imshow(viz_pipeline_cube[0, :, :], cmap='viridis')
 
-    axes[0].set_title(f'Lambda slice: {initial_lambda}')
-    axes[1].set_title(f'Lambda slice: {initial_lambda}')
+    axes[0].set_title(f'Lambda = {str(round(initial_lambda, 2))}', fontsize=30)
+    axes[1].set_title(f'Lambda = {str(round(initial_lambda, 2))}', fontsize=30)
 
     
     # Add a colorbar
     cbar = plt.colorbar(slice_plot1, ax=axes[1])
+    cbar = plt.colorbar(slice_plot1, ax=axes[0])
+
     # Add a colorbar
     # cbar = plt.colorbar(slice_plot2, ax=axes[1])
 
     # Create the slider axis and slider
     ax_slider = plt.axes([0.1, 0.1, 0.8, 0.05], facecolor='lightgoldenrodyellow')
-    slider = Slider(ax_slider, 'Lambda', 0, nLambda - 1, valinit=initial_lambda, valstep=1)
+    slider = Slider(ax_slider, 'Lambda', 0, nLambda - 1, valinit=0, valstep=1)
 
     # Update function to be called when the slider is changed
     def update(val):
@@ -116,15 +124,15 @@ def plot_two_cubes(cube_fusion, wavelength_fusion, cube_pipeline, wavelength_pip
         slice_plot2.set_data(new_slice2)
         
         # Update the color limits for the current slice
-        slice_plot1.set_clim(vmin=np.min(new_slice1), vmax=np.max(new_slice1))
-        slice_plot2.set_clim(vmin=np.min(new_slice1), vmax=np.max(new_slice1))
+        slice_plot1.set_clim(vmin=np.nanmin(new_slice1), vmax=np.nanmax(new_slice1))
+        slice_plot2.set_clim(vmin=np.nanmin(new_slice2), vmax=np.nanmax(new_slice2))
 
         # Redraw the colorbar with the new limits
         # cbar.update_normal(slice_plot1)
 
         # Update the title to show the current lambda slice
-        axes[0].set_title(f'Lambda slice: {wavelength_fusion[lambda_index]}')
-        axes[1].set_title(f'Lambda slice: {wavelength_fusion[lambda_index]}')
+        axes[0].set_title(f'Lambda = {str(round(wavelength_fusion[lambda_index], 2))}', fontsize=30)
+        axes[1].set_title(f'Lambda = {str(round(wavelength_fusion[lambda_index], 2))}', fontsize=30)
         
         # Redraw the figure canvas
         fig.canvas.draw_idle()
@@ -190,3 +198,16 @@ def plot_concatenated_cubes(cubes_list, wavelength_cubes_list):
     slider.on_changed(update)
 
     plt.show()
+
+
+def plot_maps(estimated_maps):
+    nrow = 2#estimated_maps.shape[0] // 2
+    ncols = estimated_maps.shape[0] // 2
+    print(nrow)
+    fig, axes = plt.subplots(nrows=nrow, ncols=ncols, sharex = True, sharey = True)
+
+    for i in range(nrow):
+        for j in range(ncols):
+            print(i,j)
+            m = axes[i,j].imshow(estimated_maps[i*ncols+j])
+            fig.colorbar(m, ax=axes[i,j])
